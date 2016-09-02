4733633517ebc615bfd18eae54f0fb4b8df55c3c
Carnival Fox <dyslexicangel@gmail.com>
2016-09-01 19:46:42 -0700

    added 1st outline
    What it says on the tin

---
+++ Tasty.java
@@ -0,0 +1,36 @@
+package tasty;
+/**
+ *
+ * @author Carnival Fox
+ */
+import javax.swing.JFrame;
+import java.util.Timer;
+public class Tasty {
+
+
+    public static void main(String[] args) {
+        //create a new JFrame w/ windowListener
+        //set to fullscreen
+        //find screen size
+
+        //create frame mainView
+        
+        //create dialogueBox / statBox (transparent label?) 
+
+        //create actionBar (linked list of Action objects) @todo
+        
+        //create keyListener
+        
+        //create Level (use Level's generator to place stuff) @todo
+        
+        //create Character (gonna be a complex bit of work, that) @todo
+        
+        //make everything visible that should be and not visable that shouldn't be
+        
+        //create Timer to make the plants grow when they should
+
+        //end all processes
+        //close Window     
+    }
+    
+}
